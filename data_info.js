7cbd61f540c37e9c8e6058506387d6e1837f6ae2
luzhengtai <476120532@qq.com>
2018-08-07 16:26:00 +0800

    data_jyq.js

---
+++ data_info.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+}
+var useInfo={
+  "code": 200,
+  "msg": "请求成功",
+  "data": {
+    "userId": 100001,
+    "userKey": "1456464646",
+    "account": null,
+    "nick": "owner",
+    "avatar": null,
+    "mobile": null,
+    "mobileBindStatus": 0,
+    "email": "owner_luguo@qq.com",
+    "emailBindStatus": 1,
+    "weChat": null,
+    "weChatBindStatus": 0,
+    "isOpenWeChatReminder": false,
+    "lockScreenPassword": null,
+    "isDefaultLockScreen": false,
+    "isOAuthQQ": false,
+    "isOAuthWeChat": false,
+    "syncSettings": null,
+    "version": "0",
+    "closeWarningBoxTime": "2017/10/09 12: 00",
+    "isEmptyPassword": false,
+    "lockScreenType": 0,
+  }
+}
+module.exports.useInfo = useInfo;
